0f5997a39af203c9f5e7f6711a086210fed503d8
macotoikarashi <macoto.closet.studio@gmail.com>
2023-06-16 17:40:39 +0900

    fine-tuning

--- jaToEnLayout.py
+++ jaToEnLayout.py
@@ -70,6 +70,7 @@ async def main(page: ft.Page):
 
     data = DialogueData()
 
+
     loadingBar = ft.Column(controls=[
         ft.Text("Now Processing...", style="headlineSmall"),
         ft.ProgressBar(width=400, color="amber", bgcolor="#eeeeee"),
@@ -81,7 +82,8 @@ async def main(page: ft.Page):
         userAnswer.current.disabled = sw
         ansBtn.current.disabled = sw if sw else not userAnswer.current.value
 
-    def resetWhenError():
+    async def resetWhenError():
+        # for Errors at generating Question
         disableWhenLoading(False)
         q_loading.current.visible = False
         q.current.visible = False
@@ -95,9 +97,25 @@ async def main(page: ft.Page):
         correctEx.current.value = ""
         aEvaluation.current.value = ""
         page.dialog = ft.AlertDialog(
-            title=ft.Text("APIエラーのため、リセットしました"), on_dismiss=lambda e: print("Dialog dismissed!")
+            title=ft.Text("APIエラーのため、リセットしました"), on_dismiss=lambda e: print("Dialog dismissed!"),
+            open=True
         )
+        await page.update_async()
 
+    async def retryWhenError():
+        # for Errors at generating Evaluation
+        disableWhenLoading(False)
+        a_loading.current.visible = False
+        a.current.visible = False
+        aJa.current.value = ""
+        aUser.current.value = ""
+        correctEx.current.value = ""
+        aEvaluation.current.value = ""
+        page.dialog = ft.AlertDialog(
+            title=ft.Text("APIエラー リトライしてみてください"), on_dismiss=lambda e: print("Dialog dismissed!"),
+            open=True
+        )
+        await page.update_async()
 
 
     
@@ -108,6 +126,7 @@ async def main(page: ft.Page):
             keywords.current.value = ",".join(cr.randomKeywords(3))
         
         disableWhenLoading()
+        qTxt.current.controls = []
         q_loading.current.visible = True
         q.current.visible = False
         a.current.visible = False
@@ -118,18 +137,19 @@ async def main(page: ft.Page):
         # dia_json = await ph.returnDialogue(keywords.current.value)
         dia_obj = cr.returnDialogue(keywords.current.value, apikey.current.value)
         if dia_obj["error"]:
-            resetWhenError()
+            await resetWhenError()
             return False
+        print("dia:\n",dia_obj["response"])
         dia_json = dia_obj["response"].choices[0].message.content
         # trans_json = await ph.returnJapanese(dia_json)
         trans_obj = cr.returnJapanese(dia_json, apikey.current.value)
         if trans_obj["error"]:
-            resetWhenError()
+            await resetWhenError()
             return False
+        print("trans:\n",trans_obj["response"])
         trans_json = trans_obj["response"].choices[0].message.content
         data.set(dia_json,trans_json)
         data.set_question()
-        qTxt.current.controls = []
         qTxt.current.controls = makeDiaText(data.q_dict["dialogue"])
         disableWhenLoading(False)
         q_loading.current.visible = False
@@ -154,6 +174,7 @@ async def main(page: ft.Page):
                         ft.Text(speaker, width=100,text_align="CENTER", selectable=True),
                         ft.Text(
                             dia[i]["content"][j],
+                            width=500,
                             weight=ft.FontWeight.BOLD if isBold else ft.FontWeight.NORMAL,
                             selectable=True
                         )
@@ -170,20 +191,28 @@ async def main(page: ft.Page):
 
         eval_obj = cr.returnEvaluation(situation=data.q_dict["status"]["situation"],question=data.q_japanese(), correctEx=data.q_english, answer=userAnswer.current.value, key=apikey.current.value)
         if eval_obj["error"]:
-            resetWhenError()
+            await retryWhenError()
             return False
+        print("eval:\n",eval_obj["response"])
         eval = eval_obj["response"].choices[0].message.content
-        eval_dict = json.loads(eval)
-        aJa.current.value = data.q_japanese()
-        aUser.current.value = userAnswer.current.value
-        aUser.current.color = "green" if eval_dict["isgood"] else "red"
-        correctEx.current.value = data.dia_dict["dialogue"][data.x]["content"][data.y]
-        aEvaluation.current.value = eval_dict["evaluate"]
-        disableWhenLoading(False)
-        a_loading.current.visible = False
-        a.current.visible = True
+        try:
+            eval_dict = json.loads(eval)
+            aJa.current.value = data.q_japanese()
+            aUser.current.value = userAnswer.current.value
+            aUser.current.color = "green" if eval_dict["isgood"] else "red"
+            correctEx.current.value = data.dia_dict["dialogue"][data.x]["content"][data.y]
+            aEvaluation.current.value = eval_dict["evaluate"]
+            disableWhenLoading(False)
+            a_loading.current.visible = False
+            a.current.visible = True
+            await page.update_async()
+            await page.scroll_to_async(offset=-1.0, duration=300)
+        except json.JSONDecodeError as e:
+            await retryWhenError()
+
+    async def controlGenBtn(e):
+        genBtn.current.disabled = not apikey.current.value
         await page.update_async()
-        await page.scroll_to_async(offset=-1.0, duration=300)
 
     async def controlAnsBtn(e):
         ansBtn.current.disabled = not userAnswer.current.value
@@ -196,9 +225,9 @@ async def main(page: ft.Page):
                 height=50,
             ),
             ft.Text("ひとこと英訳問題を、AIが出題してくれます。Let's try!",style=ft.TextThemeStyle.TITLE_LARGE),
-            ft.TextField(ref=apikey, label="OpenAI API key", width=600),
+            ft.TextField(ref=apikey, label="OpenAI API keyを入力", width=600, password=True, on_change=controlGenBtn),
             ft.TextField(ref=keywords, label="Input keyword(s)/キーワードを入力", width=600),
-            ft.ElevatedButton(ref=genBtn, text="問題を作成", on_click=genDia),
+            ft.ElevatedButton(ref=genBtn, text="問題を作成", on_click=genDia, disabled=True),
             ft.Row(ref=q_loading,controls=[loadingBar], visible=False),
             ft.Column(ref=q,controls=[
                 ft.Divider(),
